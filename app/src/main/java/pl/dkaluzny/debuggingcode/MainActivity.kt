f2fc28de30e6bfea79dd848639dc2e497c9cc027
kalug89 <kaluzny_david@hotmail.com>
2018-08-16 21:47:45 +0200

    Debug and TODO

--- app/src/main/java/pl/dkaluzny/debuggingcode/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/debuggingcode/MainActivity.kt
@@ -8,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        add(10, 12)
+    }
+
+    //Todo: Write new function
+    //Todo: Write more function
+    fun add(firstNum: Int, secondNum: Int) {
+        println("Sum is: ${firstNum + secondNum}")
     }
 }
