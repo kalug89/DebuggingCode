039a755cda806666d268d02919ea2baab49644f7
kalug89 <kaluzny_david@hotmail.com>
2018-08-16 21:54:13 +0200

    Change activity

--- app/src/main/java/pl/dkaluzny/debuggingcode/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/debuggingcode/MainActivity.kt
@@ -1,9 +1,10 @@
 package pl.dkaluzny.debuggingcode
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
